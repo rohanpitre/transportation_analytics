7c7e3d34b62c542585b6ebad34d238fd64a21aa9
Sasha <as4511@columbia.edu>
2016-12-07 10:12:11 -0500

    made delta's different for latitude and longitude. Fixed the matrix

--- zones.py
+++ zones.py
@@ -1,43 +1,49 @@
 import csv
 import numpy as np
 from datetime import datetime
+import time
 from sympy.solvers import solve
 from sympy import Symbol
 
 
-
-
-#assigns each pickup to the closest point. delta is the distance between points
-#makes max_lat, max_long the largest index in the grid
+#assigns each pickup to the closest point. delta is the distance between points in miles
+#returns max index of lat and lon points
 def assign_zones(max_lat, min_lat, max_lon, min_lon, delta, data):
 
+	#latitude and longitude degrees are translated into miles differently
+	#calculate specific deltas
 	delta_lat = delta / 69
 	delta_lon = delta / 52.49
 	print("delta_lat ", delta_lat)
 	print("delta_lon ", delta_lon)
+
+	#create two arrays that list the coordinates of points that pickups will be assigned to
+	#from minimum to maximum with step delta
 	lat_r = np.arange(min_lat, max_lat, delta_lat)
 	lon_r = np.arange(min_lon, max_lon, delta_lon)
+	#add the max values that arrange omits to get the full list of points.
+	#notice that distance between the last two points will be smaller than delta
 	lat_range = np.append(lat_r, max_lat)
 	lon_range = np.append(lon_r, max_lon)
-	#prints the list of points
 	print("lat range:", lat_range)
 	print("lon range:", lon_range)
 
-
-	#line[3] and line[4] are the coordinates of closest point
+	#for each tuple line[3] and line[4] are the coordinates of closest point
 	#line[5] and line[6] are numbered coordinates of closest point aka (0,1), (1,0) etc
+	
+	#dictionaries that will help translate coordinates into indeces
 	lat_points = {}
 	i=0
 	for lat in lat_range:
 		lat_points[lat]=i
 		i += 1
-	
 	lon_points = {}
 	j=0
 	for lon in lon_range:
 		lon_points[lon]=j
 		j += 1
 
+	#for each tuple fill in line[3], [4], [5], [6] with correct values
 	for line in data:
 		line.append(0)
 		line.append(0)
@@ -53,7 +59,9 @@ def assign_zones(max_lat, min_lat, max_lon, min_lon, delta, data):
 				line[4] = lon
 				line[6] = lon_points[lon]
 
+	#returns max index of lat and lon points
 	return [i,j]
+
 
 
 #sample rectangle in Manhattan
@@ -63,7 +71,11 @@ min_lat = 40.711246
 max_lon = -73.977716
 min_lon = -74.008615
 
+#delta in miles
+delta = 0.2
+print("delta", delta, " miles")
 
+#convert csv file into a 2D array: datetime, latitude, longtitude
 data = []
 with open('small_uber_dataset.csv', 'r') as f:
 	count = 0
@@ -74,29 +86,21 @@ with open('small_uber_dataset.csv', 'r') as f:
 		split_line = line.split(',')
 		date_str, lat_str, lon_str, base_str = split_line
 		new_line=[None]*3
-		if float(lat_str) < max_lat and float(lat_str) > min_lat and float(lon_str) < max_lon and float(lon_str) > min_lon:
-			new_line[0] = datetime.strptime(date_str, '%m/%d/%y %H:%M')
+		d=datetime.strptime(date_str, '%m/%d/%y %H:%M')
+		if d.hour > 16 and d.hour < 18 and float(lat_str) < max_lat and float(lat_str) > min_lat and float(lon_str) < max_lon and float(lon_str) > min_lon:
+			new_line[0] = d
 			new_line[1] = float(lat_str)
 			new_line[2] = float(lon_str)
 		if (not all(x is None for x in new_line)):
 			data.append(new_line[0:3])
 
-#print("data", data)
-#print(max_lat, min_lat, max_lon, min_lon)
-
-#delta in miles
-delta = 0.2
-print("delta", delta, " miles")
+#assign zones will alter the data array and return max indeces for lat and lon
 [max_x, max_y] = assign_zones(max_lat, min_lat, max_lon, min_lon, delta, data)
-np_data = np.array(data)
 
-#max_x = max(max([np_data[:,6]]))
-#[np_data[:,5]]
-
+#create a matrix where value at i,j is the number of pickups at (i,j) point
 print("density matrix", max_x, "by ", max_y)
-
-
 density = np.matrix(np.zeros(shape=(max_x, max_y)))
+
 for line in data:
 	density.itemset((line[5], line[6]), density.item(line[5], line[6])+1)
 print(density)
